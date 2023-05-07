3a864885acd8f3422024c6da80f20ceb80866745
stanken_a <artyemst3@mail.ru>
2023-05-07 15:56:08 +0300

    fix: fix cors again

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -4,6 +4,8 @@ const REGEXP_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9
 const allowedCors = [
   'http://stankena-mesto.nomoredomains.monster',
   'https://stankena-mesto.nomoredomains.monster',
+  'https://api.stankena-mesto.nomoredomains.monster',
+  'https://api.stankena-mesto.nomoredomains.monster',
   'localhost:3000',
 ];
 
