4f5ea98655abe4ee986d7791c826f9e876866e60
stanken_a <artyemst3@mail.ru>
2023-05-08 14:58:00 +0300

    feat: add crash test and README

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(requestLogger);
 app.use(cors);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', routes);
 
 app.use(errorLogger);
