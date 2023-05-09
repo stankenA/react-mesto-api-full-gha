7ddb0b53059383ca828538bdab4bb3ae93ccbfde
stanken_a <artyemst3@mail.ru>
2023-05-09 13:02:00 +0300

    feat: add limiter and helmet for routes in back

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const routes = require('./routes/routes');
 const сentralizedErrors = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -17,9 +19,18 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   family: 4,
 });
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(requestLogger);
 app.use(cors);
 
+app.use(limiter);
+app.use(helmet());
 app.use('/', routes);
 
 app.use(errorLogger);
